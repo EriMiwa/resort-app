744f6d1ef3a7f8ab31b71b404c10bcb56348ba31
Erika Miwa <erikamiwa612@gmail.com>
2020-03-22 02:15:17 -0700

    implement room size filter and extras filter

--- src/context.js
+++ src/context.js
@@ -55,7 +55,7 @@ class RoomProvider extends Component {
 
   handleChange = (e) => {
     const target = e.target;
-    const value = e.type === 'checkbox' ? target.checked : target.value
+    const value = target.type === 'checkbox' ? target.checked : target.value
     const name = e.target.name;
 
     this.setState({
@@ -89,10 +89,16 @@ class RoomProvider extends Component {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity)
     }
 
-    
     tempRooms = tempRooms.filter(room => room.price <= price)
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+    
+    if(breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
 
-
+    if(pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
 
     this.setState({
       sortedRooms: tempRooms
