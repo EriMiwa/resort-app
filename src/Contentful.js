319d94c3f864be7187b480fb4aa590d444cf693f
Erika Miwa <erikamiwa612@gmail.com>
2020-03-22 22:32:59 -0700

    add .env file

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-  space: "tspilr9w77n6",
-  accessToken: "0EhyAWWREDu206PzxIOLvBeRGltYNj3GA7CdAuFz8GM"
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
